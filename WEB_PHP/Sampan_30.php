a69c0580fe03c6c6d470138150129f35fc1132e1
sampan11 <45837589+sampan6108111007@users.noreply.github.com>
2020-08-27 04:23:15 +0700

    Add files via upload

---
+++ WEB_PHP/Sampan_30.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<title>SCOURCE CODE</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body id="myPage">
+    <style>
+        @font-face {
+            font-family: TLWGTypewriter;
+            src: url('rsp_tlwgtypewriter.ttf');
+
+        }
+    </style>
+    <!-- Navbar -->
+    <div class="w3-top">
+        <div class="w3-bar w3-theme-d2 w3-left-align">
+
+            <a href="https://github.com/sampan6108111007" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>PROFILE</a>
+            <a href="index.php" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>INDEX</a>
+            <div class="w3-dropdown-hover ">
+                <button class="w3-button" title="Notifications"> pyramid_NO.1.- pyramid_NO.12. <i class="fa fa-caret-down"></i></button>
+                <div class="w3-dropdown-content w3-card-4 w3-bar-block">
+
+                    <a href="Sampan_01.php?line=4" class="w3-bar-item w3-button">pyramid_NO.1.</a>
+                    <a href="Sampan_02.php?line=5" class="w3-bar-item w3-button">pyramid_NO.2.</a>
+                    <a href="Sampan_03.php?line=5" class="w3-bar-item w3-button">pyramid_NO.3.</a>
+                    <a href="Sampan_04.php?line=5" class="w3-bar-item w3-button">pyramid_NO.4.</a>
+                    <a href="Sampan_05.php?line=5" class="w3-bar-item w3-button">pyramid_NO.5.</a>
+                    <a href="Sampan_06.php?line=5" class="w3-bar-item w3-button">pyramid_NO.6.</a>
+                    <a href="Sampan_07.php?line=5" class="w3-bar-item w3-button">pyramid_NO.7.</a>
+                    <a href="Sampan_08.php?line=5" class="w3-bar-item w3-button">pyramid_NO.8.</a>
+                    <a href="Sampan_09.php?line=5" class="w3-bar-item w3-button">pyramid_NO.9.</a>
+                    <a href="Sampan_10.php?line=5" class="w3-bar-item w3-button">pyramid_NO.10.</a>
+                    <a href="Sampan_11.php?line=7" class="w3-bar-item w3-button">pyramid_NO.11.</a>
+                    <a href="Sampan_12.php?line=5" class="w3-bar-item w3-button">pyramid_NO.12.</a>
+
+                </div>
+            </div>
+
+            <div class="w3-dropdown-hover ">
+                <button class="w3-button" title="Notifications">pyramid_NO.13.- pyramid_NO.24. <i class="fa fa-caret-down"></i></button>
+                <div class="w3-dropdown-content w3-card-4 w3-bar-block">
+
+                    <a href="Sampan_13.php?line=5" class="w3-bar-item w3-button">pyramid_NO.13.</a>
+                    <a href="Sampan_14.php?line=4" class="w3-bar-item w3-button">pyramid_NO.14.</a>
+                    <a href="Sampan_15.php?line=4" class="w3-bar-item w3-button">pyramid_NO.15.</a>
+                    <a href="Sampan_16.php?line=4" class="w3-bar-item w3-button">pyramid_NO.16.</a>
+                    <a href="Sampan_17.php?line=7" class="w3-bar-item w3-button">pyramid_NO.17.</a>
+                    <a href="Sampan_18.php?line=4" class="w3-bar-item w3-button">pyramid_NO.18.</a>
+                    <a href="Sampan_19.php?line=5" class="w3-bar-item w3-button">pyramid_NO.19.</a>
+                    <a href="Sampan_20.php?line=7" class="w3-bar-item w3-button">pyramid_NO.20.</a>
+                    <a href="Sampan_21.php?line=5" class="w3-bar-item w3-button">pyramid_NO.21.</a>
+                    <a href="Sampan_22.php?line=5" class="w3-bar-item w3-button">pyramid_NO.22.</a>
+                    <a href="Sampan_23.php?line=5" class="w3-bar-item w3-button">pyramid_NO.23.</a>
+                    <a href="Sampan_24.php?line=5" class="w3-bar-item w3-button">pyramid_NO.24.</a>
+                </div>
+            </div>
+
+            <div class="w3-dropdown-hover ">
+                <button class="w3-button" title="Notifications">pyramid_NO.25.- pyramid_NO.36. <i class="fa fa-caret-down"></i></button>
+                <div class="w3-dropdown-content w3-card-4 w3-bar-block">
+
+                    <a href="Sampan_25.php?line=5" class="w3-bar-item w3-button">pyramid_NO.25.</a>
+                    <a href="Sampan_26.php?line=5" class="w3-bar-item w3-button">pyramid_NO.26.</a>
+                    <a href="Sampan_27.php?line=5" class="w3-bar-item w3-button">pyramid_NO.27.</a>
+                    <a href="Sampan_28.php?line=3" class="w3-bar-item w3-button">pyramid_NO.28.</a>
+                    <a href="Sampan_29.php?line=3" class="w3-bar-item w3-button">pyramid_NO.29.</a>
+                    <a href="Sampan_30.php?line=5" class="w3-bar-item w3-button">pyramid_NO.30.</a>
+                    <a href="Sampan_31.php?line=5" class="w3-bar-item w3-button">pyramid_NO.31.</a>
+                    <a href="Sampan_32.php?line=5" class="w3-bar-item w3-button">pyramid_NO.32.</a>
+                    <a href="Sampan_33.php?line=3" class="w3-bar-item w3-button">pyramid_NO.33.</a>
+                    <a href="Sampan_34.php?line=5" class="w3-bar-item w3-button">pyramid_NO.34.</a>
+                    <a href="Sampan_35.php?line=5" class="w3-bar-item w3-button">pyramid_NO.35.</a>
+                    <a href="Sampan_36.php?line=5" class="w3-bar-item w3-button">pyramid_NO.36.</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Image Header -->
+
+        <div class="w3-display-container w3-animate-opacity ">
+            <pre alt="boat" style="width:100%;min-height:450px;max-height:450px;">
+  <div class="w3-container w3-display-bottomleft w3-margin-bottom">  
+  <pre style="font-family:TLWGTypewriter;font-size:23px;">
+  <?php
+    echo $_GET["line"] . "<br>";
+    echo "<br/>";
+    for ($black=1; $black <= 5; $black++)
+    {
+        for ($sampan=5; $sampan >= $black; $sampan--)
+        {
+            echo ($sampan);
+        }
+        for ($sampan=1; $sampan <= $black; $sampan++)
+        {
+            echo ("*");
+        }
+        for ($sampan=2; $sampan <= $black; $sampan++)
+        {
+            echo ("*");
+        }
+        for ($sampan=$black; $sampan <= 5; $sampan++)
+        {
+            echo ($sampan);
+        } 
+    echo ("\n");
+    }
+    ?>
+    </pre>
+
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
+        <h4>Follow SAMPAN SU</h4>
+        <a class="w3-button w3-large w3-teal" href="https://web.facebook.com/profile.php?id=100029279708428" title="Facebook"><i class="fa fa-facebook"></i></a>
+        <a class="w3-button w3-large w3-teal" href="https://twitter.com/SuSampan" title="Twitter"><i class="fa fa-twitter"></i></a>
+        <a class="w3-button w3-large w3-teal" href="https://www.youtube.com/channel/UCfapp5DYa0jRIFlrLsrmLXw?view_as=subscriber" title="youtube"><i class="fa fa-youtube"></i></a>
+        <a class="w3-button w3-large w3-teal" href="https://www.instagram.com/sampansu11/?hl=en" title="instagram"><i class="fa fa-instagram"></i></a>
+
+    </footer>
+
+    <script>
+        // Script for side navigation
+        function w3_open() {
+            var x = document.getElementById("mySidebar");
+            x.style.width = "300px";
+            x.style.paddingTop = "10%";
+            x.style.display = "block";
+        }
+
+        // Close side navigation
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+        }
+
+        // Used to toggle the menu on smaller screens when clicking on the menu button
+        function openNav() {
+            var x = document.getElementById("navDemo");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+    </script>
+
+</body>
+
+</html>
