b894acf0c763ec34a63f6602de45fc58d49b3c31
sampan11 <45837589+sampan6108111007@users.noreply.github.com>
2020-09-03 13:26:41 +0700

    Update index.php

--- pro_northwind/index.php
+++ pro_northwind/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>PYRAMID PHP</title>
+<title>PHP</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -29,13 +29,6 @@
          
         </div>
       </div>
-
-     
-
-     
-
-
-
     </div>
 
     <!-- Image Header -->
